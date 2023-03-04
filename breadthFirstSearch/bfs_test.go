68e886e543253546867cd32b72e6c427a25625fc
dNaszta <david.nasztanovics@stylersonline.com>
2023-03-04 10:59:05 +0100

    add Breadth-first search by lib

--- breadthFirstSearch/bfs_test.go
+++ breadthFirstSearch/bfs_test.go
@@ -1,11 +1,13 @@
 package breadthFirstSearch
 
 import (
+	"fmt"
+	"github.com/dominikbraun/graph"
 	"testing"
 )
 
 func TestBreadthFirstSearch(t *testing.T) {
-	graph := map[string][]string{
+	g := map[string][]string{
 		"you":    {"alice", "bob", "claire"},
 		"bob":    {"anuj", "peggy"},
 		"alice":  {"peggy"},
@@ -16,7 +18,40 @@ func TestBreadthFirstSearch(t *testing.T) {
 		"jonny":  {},
 	}
 
-	if !search(graph, "you", "thom") {
+	if !search(g, "you", "thom") {
 		t.Errorf("It doesn't work well")
 	}
+}
+
+func TestBFS(t *testing.T) {
+	g := graph.New(graph.IntHash, graph.Acyclic())
+
+	_ = g.AddVertex(1)
+	_ = g.AddVertex(2)
+	_ = g.AddVertex(3)
+	_ = g.AddVertex(4)
+	_ = g.AddVertex(5)
+	_ = g.AddVertex(6)
+	_ = g.AddVertex(7)
+	_ = g.AddVertex(8)
+
+	_ = g.AddEdge(1, 2)
+	_ = g.AddEdge(1, 3)
+	_ = g.AddEdge(1, 4)
+	_ = g.AddEdge(3, 5)
+	_ = g.AddEdge(3, 6)
+	_ = g.AddEdge(2, 6)
+	_ = g.AddEdge(4, 7)
+	_ = g.AddEdge(4, 8)
+
+	startElement := 1
+	seachable := 4
+	_ = graph.BFS(g, startElement, func(value int) bool {
+		fmt.Println(value)
+		if value == seachable {
+			fmt.Println("Found!")
+			return true
+		}
+		return false
+	})
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module grokkingAlgorithmsGo
 
 go 1.19
+
+require github.com/dominikbraun/graph v0.16.0
