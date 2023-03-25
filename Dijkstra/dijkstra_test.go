af12ceb347862666321bb8fd1843679fc44e2913
dNaszta <david.nasztanovics@stylersonline.com>
2023-03-25 20:11:00 +0100

    Shortest path algorithm breaks

--- Dijkstra/dijkstra_test.go
+++ Dijkstra/dijkstra_test.go
@@ -1,6 +1,7 @@
 package Dijkstra
 
 import (
+	"github.com/dominikbraun/graph"
 	"reflect"
 	"testing"
 )
@@ -21,5 +22,27 @@ func TestDijekstra(t *testing.T) {
 	wanted := map[string]int{start: 0, "a": 5, "b": 2, "fin": 6}
 	if !reflect.DeepEqual(result, wanted) {
 		t.Errorf("dijekstra failed, result: %v, wanted: %v", result, wanted)
+	}
+}
+
+func TestShortestPath(t *testing.T) {
+	g := graph.New(graph.StringHash, graph.Weighted(), graph.Directed())
+
+	g.AddVertex("start")
+	g.AddVertex("a")
+	g.AddVertex("b")
+	g.AddVertex("fin")
+
+	g.AddEdge("start", "a", graph.EdgeWeight(6))
+	g.AddEdge("start", "b", graph.EdgeWeight(2))
+	g.AddEdge("a", "fin", graph.EdgeWeight(1))
+	g.AddEdge("b", "a", graph.EdgeWeight(3))
+	g.AddEdge("b", "fin", graph.EdgeWeight(5))
+
+	path, _ := graph.ShortestPath(g, "start", "fin")
+
+	wanted := []string{"start", "b", "a", "fin"}
+	if !reflect.DeepEqual(path, wanted) {
+		t.Errorf("shortest path failed, result: %v, wanted: %v", path, wanted)
 	}
 }
