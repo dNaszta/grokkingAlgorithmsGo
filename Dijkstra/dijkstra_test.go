db1e9fbd1436d116be4d616b41e72a3b05cac394
dNaszta <david.nasztanovics@stylersonline.com>
2023-03-25 19:38:54 +0100

    dijkstra algorithm fix test

--- Dijkstra/dijkstra_test.go
+++ Dijkstra/dijkstra_test.go
@@ -10,16 +10,15 @@ import (
 
 func TestDijekstra(t *testing.T) {
 	start := "start"
-	fin := "fin"
 	graph := map[string]map[string]int{
 		start: {"a": 6, "b": 2},
 		"a":   {"fin": 1},
 		"b":   {"a": 3, "fin": 5},
-		fin:   {},
+		"fin": {},
 	}
 
 	result := FindLowestCosts(graph, start)
-	wanted := map[string]int{start: 0, "a": 5, "b": 2, fin: 6}
+	wanted := map[string]int{start: 0, "a": 5, "b": 2, "fin": 6}
 	if !reflect.DeepEqual(result, wanted) {
 		t.Errorf("dijekstra failed, result: %v, wanted: %v", result, wanted)
 	}
